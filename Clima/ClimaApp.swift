c3a05a6f364c9e6191491dfcd6559c0b84e2ebca
Ernest Danials <97393666+ernest-danials@users.noreply.github.com>
2025-05-22 09:45:34 -0700

    Initial Commit

---
+++ Clima/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Clima
+//
+//  Created by Myung Joon Kang on 2025-05-22.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
