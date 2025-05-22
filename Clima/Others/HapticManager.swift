01633f0bde83db0dc6a0b3a0ec64d2649d9911cc
Ernest Danials <97393666+ernest-danials@users.noreply.github.com>
2025-05-22 09:52:14 -0700

    chore: initial project setup; extensions, HapticManager, and ScaleButtonStyle

--- Clima/Others/HapticManager.swift
+++ Clima/Others/HapticManager.swift
@@ -5,4 +5,20 @@
 //  Created by Myung Joon Kang on 2025-05-22.
 //
 
-import Foundation
+import UIKit
+
+class HapticManager {
+    static let shared = HapticManager()
+    
+    init() {}
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
--- Clima/Others/ScaleButtonStyle.swift
+++ Clima/Others/ScaleButtonStyle.swift
@@ -5,4 +5,27 @@
 //  Created by Myung Joon Kang on 2025-05-22.
 //
 
-import Foundation
+import SwiftUI
+
+struct ScaleButtonStyle: ButtonStyle {
+    let scaleAmount: CGFloat
+    let opacityAmount: Double
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? scaleAmount : 1.0)
+            .opacity(configuration.isPressed ? opacityAmount : 1.0)
+    }
+}
+
+extension View {
+    func scaleButtonStyle(scaleAmount: CGFloat = 0.98, opacityAmount: Double = 1.0) -> some View {
+        self.buttonStyle(ScaleButtonStyle(scaleAmount: scaleAmount, opacityAmount: opacityAmount))
+    }
+}
+
+extension ScrollView {
+    func prioritiseScaleButtonStyle() -> some View {
+        self.gesture(DragGesture(minimumDistance: 0), including: .all)
+    }
+}
