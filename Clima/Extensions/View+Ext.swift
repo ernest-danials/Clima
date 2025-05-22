b3ea2f6cd3c063d406a40b862cddefb5d8c1719d
Ernest Danials <97393666+ernest-danials@users.noreply.github.com>
2025-05-22 09:51:52 -0700

    chore: initial project setup; extensions, HapticManager, and ScaleButtonStyle

---
+++ Clima/Extensions/View+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  View+Ext.swift
+//  Clima
+//
+//  Created by Myung Joon Kang on 2025-05-22.
+//
+
+import Foundation
---
+++ Clima/Others/HapticManager.swift
@@ -0,0 +1,8 @@
+//
+//  HapticManager.swift
+//  Clima
+//
+//  Created by Myung Joon Kang on 2025-05-22.
+//
+
+import Foundation
