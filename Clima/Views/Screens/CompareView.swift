19310310105fbfd3a4a115b7e2918d5f8782f41d
Ernest Danials <97393666+ernest-danials@users.noreply.github.com>
2025-05-25 18:55:56 -0700

    feat: set CompareView up

---
+++ Clima/Views/Screens/CompareView.swift
@@ -0,0 +1,18 @@
+//
+//  CompareView.swift
+//  Clima
+//
+//  Created by Myung Joon Kang on 2025-05-25.
+//
+
+import SwiftUI
+
+struct CompareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompareView()
+}
