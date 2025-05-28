0ba3a7841871e10588f7fee65c7d01a3129c7598
Ernest Danials <97393666+ernest-danials@users.noreply.github.com>
2025-05-28 12:52:45 -0700

    chore: create OnboardingView with placeholder

---
+++ Clima/Views/Screens/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Clima
+//
+//  Created by Myung Joon Kang on 2025-05-28.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("OnboardingView")
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
